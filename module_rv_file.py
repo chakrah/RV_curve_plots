180037103d13a0ac6103a6ae2d87da7cfa2e2273
hritamchakraborty <hritam.chakraborty@unige.ch>
2023-05-09 10:20:32 +0200

    plotting works from class

--- module_rv_file.py
+++ module_rv_file.py
@@ -49,17 +49,26 @@ class rv_curve_class:
         #Note how when we call the true_anomaly method, we do not need to give any input parameter
         #All the parameters are attributes of the instance, so they are used in the method computationscre
         nu = self.true_anomaly()                         # get true anomaly from true_anomaly function
-        rv = self.k * np.cos(nu + self.w) + self.e * np.cos(self.w)  # compute RV curve
-        return self.t_vector, rv
+        #Note how we can add the rv attribute to the instance
+        self.rv = self.k * np.cos(nu + self.w) + self.e * np.cos(self.w)  # compute RV curve
+
+    #Let's create a new method to plot the light curve
+    def plot(self):  
+        #Let us compute the curve
+        self.rv_curve()
+        # plot the RV curve
+        plt.plot(self.t_vector, self.rv)
+        plt.xlabel("Time [d]")
+        plt.ylabel("RV [m/s]")
+        plt.show()
 
 
-rv = rv_curve_class(t0=0., p=10., e=0.6, w=np.pi/3, k=10., t_init=0., t_end=25.)
 
-# compute the RV curve
-t, rv = rv.rv_curve()
+# # compute the RV curve
+# t, rv = rv.rv_curve()
 
-# plot the RV curve
-plt.plot(t, rv)
-plt.xlabel("Time [d]")
-plt.ylabel("RV [m/s]")
-plt.show()
+# # plot the RV curve
+# plt.plot(t, rv)
+# plt.xlabel("Time [d]")
+# plt.ylabel("RV [m/s]")
+# plt.show()

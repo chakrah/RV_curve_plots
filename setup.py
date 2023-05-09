1a4a062224c06c90116d1c1d3694908f4f5ac758
hritamchakraborty <hritam.chakraborty@unige.ch>
2023-05-09 11:16:30 +0200

    Added setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+setuptools.setup(
+name= "rv_curve_jura",
+version="0.1",
+author="Hritam Chakraborty",
+author_email="Hritam.Chakraborty@unige.ch",
+description="Plot all RV curves you want!",
+packages= setuptools.find_packages(),
+install_requires=["numpy", "matplotlib],
+classifiers=["Programming Language :: Python :: 3]",
+)

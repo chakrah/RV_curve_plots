09eeb344534ff86bb195e84851e6b77858151452
hritamchakraborty <hritam.chakraborty@unige.ch>
2023-05-09 11:44:50 +0200

    Package now has license and README file

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import setuptools
+
 setuptools.setup(
 name= "rv_curve_jura",
 version="0.1",
@@ -5,6 +7,6 @@ author="Hritam Chakraborty",
 author_email="Hritam.Chakraborty@unige.ch",
 description="Plot all RV curves you want!",
 packages= setuptools.find_packages(),
-install_requires=["numpy", "matplotlib],
-classifiers=["Programming Language :: Python :: 3]",
+install_requires=["numpy", "matplotlib"],
+classifiers=["Programming Language :: Python :: 3"],
 )
